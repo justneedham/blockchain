2868dca8dc8ef5b3c6ce010d8433391d85b6279a
Justin Needham <justin.needham@alumnimail.pepperdine.edu>
2018-10-13 10:58:50 -0600

    initial files

---
+++ block.py
@@ -0,0 +1,13 @@
+block = {
+    'index': 1,
+    'timestamp': 234234234,
+    'transactions' : [
+        {
+            'sender': '3234;lkj23',
+            'recipient': 'asd;flkj3;l23kj4',
+            'amount': 5,
+        }
+    ],
+    'proof': 234234234,
+    'previous_hash': 'asd;lfjk23;lkjsdf09ads8fu023lojw3rsfd'
+}
